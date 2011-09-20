d833a29be9ba7851555d52a8c890f89e38bf5133
Feyyaz Alingan <mail@feyyaz.me>
2011-09-20 16:32:18 +0200

    adds a links test to the layout links spec file and removes logo from the _header file to the application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-  
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
+ #   return "Sample App" unless logo_html
+ #   logo_html
+ # end
+
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
